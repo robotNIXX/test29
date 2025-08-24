188f2cd390baa69987d8c0602af0c01889bd7ab2
ANdrey Baranov <randebar@gmail.com>
2025-08-24 23:21:47 +0300

    init the app

---
+++ src/api/database/migrations/2025_08_24_200221_create_car_models_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('brand_id')->constrained('car_brands');
+            $table->unique(['brand_id', 'title'], 'title_uq');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('car_models');
+    }
+};
---
+++ src/api/database/migrations/2025_08_24_200850_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('model_id')->constrained('car_models');
+            $table->foreignId('brand_id')->constrained('car_brands');
+            $table->foreignId('owner_id')->constrained('users');
+            $table->string('color')->nullable();
+            $table->string('production_year')->nullable();
+            $table->bigInteger('mileage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
